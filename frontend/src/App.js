03afb1048c48b566f89d2552e80f277b058304e7
majojukrishna03 <majojukrishna03@gmail.com>
2024-06-29 00:38:29 +0530

    updated login page with login handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Homepage'; // Import the Home component
 import Register from './components/Register'; // Import the Register component
+import Login from './components/Login'; // Import the Login component
+
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
         {/* Add more routes as needed */}
       </Routes>
     </Router>
